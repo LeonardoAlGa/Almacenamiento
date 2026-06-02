2388c1e47dc0b4428c985fba16e8b0a2eed7df23
LeonardoAlarcon <edwinisaac@comunidad.unam.mx>
2026-06-01 21:15:03 -0600

    Ejercicio 4

--- app/src/main/java/com/example/almacenamiento/MainActivity.kt
+++ app/src/main/java/com/example/almacenamiento/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.almacenamiento
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -40,6 +41,12 @@ class MainActivity : AppCompatActivity() {
         openFileInput("datos_usuario.txt").bufferedReader().use { reader->
             val text= reader.readText()
             binding.tvwContentFile.text= text
+        }
+
+        val arrayArchivos: Array<String> = fileList()
+
+        arrayArchivos.forEach { nombre ->
+            Log.i("Stored", nombre)
         }
 
 
